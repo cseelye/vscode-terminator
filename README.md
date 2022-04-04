d52916a7ca8b0f66e573e7b2278559806862a411
Carl Seelye <cseelye@gmail.com>
2022-04-04 09:00:28 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # vscode-terminator
 Launch customized terminals in VS Code
+
+This extension allows you to define custom terminals in your settings and launch them with a command from the palette (*Terminator: Launch Terminal*) or API (`vscode-terminator.launchTerminal`).
+
+## Settings
+Top level settings:  
+`terminator.env` allows you to specify environment variables to set for all terminals.  
+`terminator.terminals` is a list of terminal objects where you specify your terminal configurations.
+
+Per-terminal settings:
+| Setting   | Description |
+| --------- | ----------- |
+| name      | Friendly name for the terminal |
+| color     | Color to make the terminal icon; see https://code.visualstudio.com/api/references/theme-color#integrated-terminal-colors |
+| icon      | Icon to set for the terminal; see https://code.visualstudio.com/api/references/icons-in-labels#icon-listing |
+| message   | Initial message to show when the terminal is created |
+| shellPath | Path to the shell executable |
+| shellArgs | Args to pass to the shell executable, in exec format |
+| workDir   | Directory to start the shell in |
+| env       | Shell-specific environment variables to set |
+
+Environment variables are combines from the global list and per-terminal list, with the per-terminal list having precedence. Environment variables that are used in shellPath or shellArgs will be replaced with the values.
+
+## Example Configuration
+
+```js
+{
+    "terminator.env": {
+        "asdf": "123",
+        "qwerty": "some string"
+    },
+    "terminator.terminals": [
+        {
+            "name": "first",
+            "color": "terminal.ansiCyan",
+            "icon": "server",
+            "message": "\nThis is your important terminal\n",
+            "shellPath": "/bin/bash",
+            "shellArgs": ["${login}"],
+            "workDir": "/tmp",
+            "env": {
+                "asdf": "987",
+                "hello": "there",
+                "login": "-l"
+            }
+        }
+    ]
+}
+```
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as ts from "typescript";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,7 +15,116 @@ function registerCommands(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-function launchTerminal() {
+
+interface TerminalConfig {
+	name: string;
+	icon: string;
+	color: string;
+	message: string;
+	workDir: string;
+	env: { [key: string]: string };
+	shellPath: string;
+	shellArgs: [string];
+}
+
+function getDefinedTerminalNames() {
 	const settings = vscode.workspace.getConfiguration("terminator");
-	console.log("Launching terminal");
+	const terminalSettings = settings.get<Array<TerminalConfig>>("terminals");
+	if (terminalSettings === undefined) {
+		return [];
+	}
+	let names:Array<string> = [];
+	for (let config of terminalSettings){
+		names.push(config.name);
+	}
+	return names;
+}
+
+function getTerminalConfig(terminalName: string) {
+	const settings = vscode.workspace.getConfiguration("terminator");
+	const terminalSettings = settings.get<Array<TerminalConfig>>("terminals");
+	if (terminalSettings === undefined) {
+		return undefined;
+	}
+	for (let config of terminalSettings) {
+		if (config.name === terminalName) {
+			return config;
+		}
+	}
+}
+
+async function askUserForTerminal() {
+	let names = getDefinedTerminalNames();
+	if (names.length <= 0) {
+		vscode.window.showWarningMessage("Please define a terminal in settings", "Open Settings").then(
+			item => {
+				vscode.commands.executeCommand('workbench.action.openSettings', '@ext:cseelye.vscode-terminator');
+			}
+		);
+		return undefined;
+	}
+	let name = await vscode.window.showQuickPick(getDefinedTerminalNames(), {"placeHolder": "Select the terminal to launch"} );
+	return name;
+}
+
+async function launchTerminal() {
+	// Prompt the user to select a terminal
+	let termName = await askUserForTerminal();
+	if (termName === undefined) {
+		return;
+	}
+	launchTerminalByName(termName);
+}
+
+function substituteVariable(source:string, vars: { [key: string]: string }) : string {
+	if (source === undefined) {
+		return source;
+	}
+	let replaced = source;
+	for (let key in vars) {
+		const re = new RegExp(`\\\${${key}}`);
+		replaced = replaced.replace(re, vars[key]);
+	}
+	return replaced;
+}
+
+function launchTerminalByName(terminalName: string) {
+	// Get the glocal settings
+	const settings = vscode.workspace.getConfiguration("terminator");
+	const envSettingsGlobal: { [key: string]: string } = settings.get("env", {});
+
+	// Get the config for the selected terminal
+	let config = getTerminalConfig(terminalName);
+	if (config === undefined) {
+		return vscode.window.showErrorMessage("Could not find terminal configuration for " + terminalName);
+	}	
+	let termColor = config.color ? new vscode.ThemeColor ( config.color ) : undefined;
+	let termIcon = config.icon ? new vscode.ThemeIcon(config.icon) : undefined;
+
+	// Combine global and local env variable settings and replace any ENV variables
+	let consoleEnv:{ [key: string]: string } = {};
+	Object.assign(consoleEnv, envSettingsGlobal);
+	for (let key in config.env) {
+		consoleEnv[key] = config.env[key];
+	}
+
+	// Substitute any ENV variables in the terminal command/args
+	let shellPath = substituteVariable(config.shellPath, consoleEnv);
+	let shellArgs = config.shellArgs;
+	for (let idx=0; idx < shellArgs.length; idx++) {
+		shellArgs[idx] = substituteVariable(shellArgs[idx], consoleEnv);
+	}
+
+	// Launch the terminal
+	let term = vscode.window.createTerminal({
+		color: termColor,
+		cwd: config.workDir,
+		env: consoleEnv,
+		iconPath: termIcon,
+		name: config.name,
+		message: config.message,
+		shellPath: shellPath,
+		shellArgs: shellArgs
+	});
+	term.show(false); // false means focus on the terminal
 }
